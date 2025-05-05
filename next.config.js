90872135978c0f9b40e13132724a945b41fb5c9f
erasto kamuli jelemia <erastojelemia@gmail.com>
2025-05-05 21:37:21 +0300

    pac

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: { 
+    unoptimized: true,
+    domains: ['images.pexels.com']
   },
-  images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 module.exports = nextConfig;
